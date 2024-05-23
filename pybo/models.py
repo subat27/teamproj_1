7adaaa2f4666a9068dc66f08c4b1c7b91fc77727
subat27 <ksw1010100@gmail.com>
2024-05-23 19:15:27 +0900

    add google map api infoWindow

--- pybo/models.py
+++ pybo/models.py
@@ -1,20 +1,21 @@
 from pybo import db
 
-class Conf_Age(db.Model):
+class ConfAge(db.Model):
     createDt = db.Column(db.String(20), primary_key=True)
     ageArea = db.Column(db.String(20), primary_key=True)
     confCase = db.Column(db.Integer)
     deathCnt = db.Column(db.Integer)
     
-class Conf_Gender(db.Model):
+class ConfGender(db.Model):
     createDt = db.Column(db.String(20), primary_key=True)
     gender = db.Column(db.String(20), primary_key=True)
     confCase = db.Column(db.Integer)
     deathCnt = db.Column(db.Integer)
     
-class Conf_Local(db.Model):
+class ConfLocal(db.Model):
     createDt = db.Column(db.String(20), primary_key=True)
     localName = db.Column(db.String(20), primary_key=True)
+    localNameEn = db.Column(db.String(30))
     confCase = db.Column(db.Integer)
     incDec = db.Column(db.Integer)
     deathCnt = db.Column(db.Integer)
--- pybo/views/area_views.py
+++ pybo/views/area_views.py
@@ -1,37 +1,43 @@
 from flask import Flask, Blueprint, render_template, g, request, url_for
 from werkzeug.utils import redirect
 import pandas as pd
-from pybo.models import Conf_Local
+from pybo.models import ConfLocal
 from pybo import db
+from dotenv import load_dotenv
+import os
+from flask import json
 
 bp = Blueprint("area", __name__, url_prefix="/area")
 
 def init_data():
     df = pd.read_csv("pybo\static\data\covid_data_korea_local.csv")
     
-    covid_data_local = df[['gubun', 'deathCnt', 'defCnt', 'stdDay', 'incDec']].loc[(df["gubun"]!="합계")&(df["gubun"]!="검역")].drop_duplicates(subset=["gubun", "stdDay"]).reset_index().drop(columns=["index"])
+    covid_data_local = df[['gubun', 'deathCnt', 'defCnt', 'stdDay', 'incDec', 'gubunEn']].loc[(df["gubun"]!="합계")&(df["gubun"]!="검역")].drop_duplicates(subset=["gubun", "stdDay"]).reset_index().drop(columns=["index"])
 
     for x in covid_data_local.index:
-        temp_data =Conf_Local(localName=covid_data_local.iloc[x, 0],
+        temp_data =ConfLocal(localName=covid_data_local.iloc[x, 0],
                               deathCnt=int(covid_data_local.iloc[x, 1]),
                               confCase=covid_data_local.iloc[x, 2],
                               createDt=covid_data_local.iloc[x, 3],
-                              incDec=covid_data_local.iloc[x, 4]
+                              incDec=covid_data_local.iloc[x, 4],
+                              localNameEn=covid_data_local.iloc[x, 5]
                               )
         db.session.add(temp_data)
     db.session.commit()
 
+
 def find_data(date):
-    return Conf_Local.query.filter_by(createDt=date).all()
+    #ConfLocal.query.order_by(ConfLocal.createDt.desc())
+    return ConfLocal.query.filter_by(createDt=date).all()
 
 @bp.route("show_data", methods=["GET", "POST"])
 def show_data():
     #if request.method == "GET":
 
-    api_key = ""
-    with open("pybo\static\data\\api_keys.txt", "r", encoding="utf-8") as f:
-        api_key = f.readline()
+    if load_dotenv() :
+        api_key = os.getenv('GOOGLE_API_KEY')
         
-    return render_template("area/test.html", api_key=api_key)
+        return render_template("area/test.html", api_key=api_key, covid_data=find_data('2023-08-31'))
+        
     #return render_template("area/showArea.html", date=request.form["date"])
     #return render_template("showArea.html", find_data())
