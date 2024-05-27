b27c46db5f50f3f421325cf89f4b7bdaa64b09aa
mbc <dksdudwls888@naver.com>
2024-05-27 17:13:36 +0900

    그래프 출력 시도

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -15,10 +15,11 @@ def create_app():
     migrate.init_app(app,db)
     from . import models
 
-    from .views import main_views, age_views, area_views, gender_views
+    from .views import main_views, age_views, area_views, gender_views, overseas_views  
     app.register_blueprint(main_views.bp)
     app.register_blueprint(age_views.bp)
     app.register_blueprint(area_views.bp)
-    app.register_blueprint(gender_views.bp)    
+    app.register_blueprint(gender_views.bp)
+    app.register_blueprint(overseas_views.bp)    
 
     return app
