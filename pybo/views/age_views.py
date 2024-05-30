4b87691e6507b1eba6239be5226d66a5d13b5bdd
mbc <hisassac@gmail.com>
2024-05-30 17:11:32 +0900

    20240530

--- pybo/views/age_views.py
+++ pybo/views/age_views.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from pybo.models import ConfAge
+from pybo import db
 
 mpl.rc("font", family="Malgun Gothic")
 
@@ -12,43 +14,53 @@ mpl.rc("font", family="Malgun Gothic")
 
 bp = Blueprint("age", __name__, url_prefix="/age")
 
-@bp.route("/init_global_data")
+@bp.route("getData")
+def getData():
+    
+
+    return ""
+
+@bp.route("/create_age_chart_img")
 def init_global_data():
-    df = pd.read_csv("pybo\static\data\COVID19-global.csv")
 
-    df1 = df.copy()
-    df1=df1.drop(columns=df1.columns[[1, 3, 6, 7]])
+    covid_data_age = ConfAge.getColumnList(db.session)
 
-    df1.rename(columns={'Date_reported':'날짜',
-                        'Country':'국가',
-                        'New_cases':'신규확진자',
-                        'Cumulative_cases':'누적확진자'}, inplace=True)
+    df_age = pd.DataFrame(covid_data_age)
+    df_age.rename(columns={
+        "deathCnt" : "사망자수", 
+        "ageArea" : "연령", 
+        "confCase" : "신규확진자", 
+        "createDt" : "날짜"
+    }, inplace=True)
+    df_age["날짜"] = pd.to_datetime(df_age["날짜"])
 
-    df1["날짜"] = pd.to_datetime(df1["날짜"])
-       
-    for area in df1["국가"].unique():
-        saveFile(df1, area)
+    saveFile(df_age)
 
-    return redirect("")
+    return ""
 
 
-def saveFile(df, area):
-    filename = area + ".png"
+
+def saveFile(df):
+    filename = "age.png"
     upload_path = makedirectory()
     path = os.path.join(upload_path, filename)
     path = path.replace("\\", "/")
 
-    filter=df["국가"]==area 
-    temp_df = df[filter]
-    temp_df.plot(x="날짜", y=["신규확진자"])
-    plt.savefig(path)
+    plt.figure(figsize=(13, 8), layout='constrained')
 
+    for age in df["연령"].unique():
+        filter = df["연령"]==age
+        plt.plot(df[filter]["날짜"], df[filter]["신규확진자"], label=age)
+
+    plt.legend(df["연령"].unique())
+    plt.savefig(path)
+    plt.close()
     idx = path.find("/static/img")
 
     return path[idx:]
 
 def makedirectory():
-    UPLOAD_DIR="pybo/static/img"
+    UPLOAD_DIR="pybo/static/charts"
     name_ymd = datetime.now().strftime("%Y%m%d")
 
     new_dir_path = os.path.join(UPLOAD_DIR, name_ymd)
@@ -56,4 +68,4 @@ def makedirectory():
     if not os.path.exists(new_dir_path):
         os.mkdir(new_dir_path)
 
-    return new_dir_path
+    return new_dir_path    
